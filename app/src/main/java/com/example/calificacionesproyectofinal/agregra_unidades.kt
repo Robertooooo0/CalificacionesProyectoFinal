e8e68dea9554d0f4dfbdde18595dd03f94f87988
Roberto Carranza <126268704+Robertooooo0@users.noreply.github.com>
2023-11-10 08:38:55 -0600

    Agrega con los paraametros correctos las unidades

--- app/src/main/java/com/example/calificacionesproyectofinal/agregra_unidades.kt
+++ app/src/main/java/com/example/calificacionesproyectofinal/agregra_unidades.kt
@@ -19,21 +19,31 @@ class agregra_unidades : AppCompatActivity() {
         databaseManager = DatabaseManager(this) // Inicializa tu DatabaseManager
 
         btnguardarunidades.setOnClickListener {
-            val nombreUnidad = txtguardarunidades.text.toString()
-            // Aquí deberías obtener el ID del semestre, por ejemplo:
-            val idSemestre = 0 // Reemplaza esto por la lógica para obtener el ID del semestre
+            val nombreUnidad = txtguardarunidades.text.toString().trim()
 
-            // Guardar en la base de datos
-            databaseManager.open()
-            val result = databaseManager.insertUnidad(nombreUnidad, idSemestre)
-            databaseManager.close()
+            if (nombreUnidad.isNotEmpty()) { // Verifica si el campo no está vacío
+                val idSemestre = 0 // Reemplaza esto por la lógica para obtener el ID del semestre
 
-            if (result != -1L) {
-                // Éxito al guardar
-                Toast.makeText(this, "Unidad guardada correctamente", Toast.LENGTH_SHORT).show()
+                // Guardar en la base de datos
+                databaseManager.open()
+                val result = databaseManager.insertUnidad(nombreUnidad, idSemestre)
+                databaseManager.close()
+
+                if (result != -1L) {
+                    // Éxito al guardar
+                    Toast.makeText(this, "Unidad guardada correctamente", Toast.LENGTH_SHORT).show()
+                    onBackPressed()
+                } else {
+                    // Error al guardar
+                    Toast.makeText(this, "Error al guardar la unidad", Toast.LENGTH_SHORT).show()
+                }
             } else {
-                // Error al guardar
-                Toast.makeText(this, "Error al guardar la unidad", Toast.LENGTH_SHORT).show()
+                // Campo vacío, muestra un mensaje al usuario
+                Toast.makeText(
+                    this,
+                    "Por favor, ingresa el nombre de la unidad",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
